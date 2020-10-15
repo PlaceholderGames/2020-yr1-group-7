3ee376f34b9360cc2dce09ec11ebb4de9d59c2c5
nimshi89 <krajcsirik20@gmail.com>
2020-10-15 17:59:55 +0100

    added a method to render an image for the player and created a map tile system

--- Game.py
+++ Game.py
@@ -2,24 +2,28 @@ from Player import Player
 import pygame
 from Game_States import GameStates
 
+SCALE = 64
 
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.objects = []
         self.Game_States = GameStates.NONE
+        self.map = []
 
     def set_up(self):
         player = Player(1, 1)
         self.player = player
         self.objects.append(player)
         self.Game_States = GameStates.RUNNING
+        self.load_map("map1")
         print("do the setup")
 
     def update(self):
         self.screen.fill((0, 0, 0))
         print("updating")
         self.handle_events()
+        self.render_the_map(self.screen)
 
         for object in self.objects:
             object.render(self.screen)
@@ -40,3 +44,28 @@ class Game:
                     self.player.update_position(0, -1)
                 elif event.key == pygame.K_DOWN:
                     self.player.update_position(0, 1)
+
+    def load_map(self, file_name):
+        with open("maps/" + file_name + ".txt") as map_file:
+            for line in map_file:
+                tile = []
+                for i in range(0, len(line) - 1, 2):
+                    tile.append(line[i])
+                    
+                self.map.append(tile)
+
+    def render_the_map(self, screen):
+        tile_y = 0
+        for line in self.map:
+            tile_x = 0
+            for tile in line:
+                image = images_for_map[tile]
+                rect = pygame.Rect(tile_x * SCALE, tile_y * SCALE, SCALE, SCALE)
+                screen.blit(image, rect)
+                tile_x += 1
+            tile_y += 1
+
+
+images_for_map = {
+    "G": pygame.transform.scale(pygame.image.load("images\_tile1.png"), (SCALE, SCALE))
+}
--- Player.py
+++ Player.py
@@ -1,21 +1,23 @@
 import pygame
-SCALE = 15
+SCALE = 64
 
 class Player:
     def __init__(self, xpos, ypos): #Initializin Player
         self.position = [xpos, ypos]
+        self.image = pygame.image.load("images\player.png")
+        self.image = pygame.transform.scale(self.image, (SCALE, SCALE))
+        self.rect = pygame.Rect(self.position[0] * SCALE, self.position[1] * SCALE, SCALE, SCALE)
+
         print("Player")
 
     def update(self):       #This is where the player will be updtated
         print("Update")
 
-    def render(self, screen):       #Drwaing the player to the screen, of course this is just temporeraly
-        pygame.draw.rect(screen, (255, 255, 255), (self.position[0] * SCALE, self.position[1] * SCALE, SCALE, SCALE), 6)
-
-
     def update_position(self, xpos, ypos):
         self.position[0] += xpos
         self.position[1] += ypos
+        self.rect = pygame.Rect(self.position[0] * SCALE, self.position[1] * SCALE, SCALE, SCALE)
 
-
+    def render(self, screen):       #Drwaing the player to the screen, of course this is just temporeraly
+        screen.blit(self.image, self.rect)
 
